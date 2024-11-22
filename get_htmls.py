99e01c7c804ee4e84e9553854c1ca7f6a6d1868b
NorwegianIcecube <patkar07.pk@gmail.com>
2024-11-22 13:22:03 +0100

    added feature of skipping CELEX IDs already present in HTMLs folder in case of early abortion

--- get_htmls.py
+++ get_htmls.py
@@ -1,10 +1,17 @@
 from eurlex import get_html_by_celex_id
 import json
 from tqdm import tqdm
+import os
 
-# Retrieve and parse the document with CELEX ID "32019R0947" into a Pandas DataFrame
+# List of CELEX IDs in HTMLs folder, used to avoid re-downloading the same documents
+html_files = os.listdir('HTMLs')
+html_celex_ids = [html_file.split('.')[0] for html_file in html_files]
+
+
 celex_ids = json.load(open('celex_ids.json'))
 for id in tqdm(range(len(celex_ids))):
+    if celex_ids[id] in html_celex_ids:
+        continue
     celex_id = celex_ids[id]
     html = get_html_by_celex_id(celex_id)
 
